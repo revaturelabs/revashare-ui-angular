d9477ba149e40c5f294650e1ecb84d116b568507
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-20 10:03:56 -0500

    Cleanup global variables inside routing.

--- angular-ui/routing.js
+++ angular-ui/routing.js
@@ -1,101 +1,101 @@
+(function(ng) {
+    var login;
+    var message;
+    var profile;
+    var schedule;
+    var signup;
+    var user;
+    var welcome;
 
-angular.module("revashare").config(function ($stateProvider, $urlRouterProvider) {
-    
-    $urlRouterProvider.otherwise("/welcome");
+    ng.module("revashare").config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
 
-    $stateProvider
-        .state("welcome", welcome)
-        
-        .state("signup", signup)
-        .state("login", login)
-        .state("profile", profile)
+        $urlRouterProvider.otherwise("/welcome");
 
-        .state("schedule", schedule)
-        .state("user", user)
-        .state("message", message);
+        $stateProvider
+            .state("welcome", welcome)
+            .state("signup", signup)
+            .state("login", login)
+            .state("profile", profile)
+            .state("schedule", schedule)
+            .state("user", user)
+            .state("message", message);
 
-});
+    }]);
 
-
-var welcome = {
-    url: "/welcome",
-    views: {
-        "main": {
-            templateUrl: "routes/welcome/index.html",
-            controller: "welcome_controller",
-            controllerAs: "vm"
+    welcome = {
+        url: "/welcome",
+        views: {
+            "main": {
+                templateUrl: "routes/welcome/index.html",
+                controller: "welcome_controller",
+                controllerAs: "vm"
+            }
         }
-    }
-}
+    };
 
-
-var signup = {
-    url: "/signup",
-    views: {
-        "main": {
-            templateUrl: "routes/signup/index.html",
-            controller: "signup_controller",
-            controllerAs: "vm"
+    signup = {
+        url: "/signup",
+        views: {
+            "main": {
+                templateUrl: "routes/signup/index.html",
+                controller: "signup_controller",
+                controllerAs: "vm"
+            }
         }
-    }
-};
+    };
 
-
-var login = {
-    url: "/login",
-    views: {
-        "main": {
-            templateUrl: "routes/login/index.html",
-            controller: "login_controller",
-            controllerAs: "vm"
+    login = {
+        url: "/login",
+        views: {
+            "main": {
+                templateUrl: "routes/login/index.html",
+                controller: "login_controller",
+                controllerAs: "vm"
+            }
         }
-    }
-};
+    };
 
-
-var profile = {
-    url: "/profile",
-    views: {
-        "main": {
-            templateUrl: "routes/profile/index.html",
-            controller: "profile_controller",
-            controllerAs: "vm"
+    profile = {
+        url: "/profile",
+        views: {
+            "main": {
+                templateUrl: "routes/profile/index.html",
+                controller: "profile_controller",
+                controllerAs: "vm"
+            }
         }
-    }
-}
+    };
 
-
-var schedule = {
-    url: "/schedule",
-    views: {
-        "main": {
-            templateUrl: "routes/schedule/index.html",
-            controller: "schedule_controller",
-            controllerAs: "vm"
+    schedule = {
+        url: "/schedule",
+        views: {
+            "main": {
+                templateUrl: "routes/schedule/index.html",
+                controller: "schedule_controller",
+                controllerAs: "vm"
+            }
         }
-    }
-}
+    };
 
-
-var user = {
-    url: "/user",
-    views: {
-        "main": {
-            templateUrl: "routes/user/index.html",
-            controller: "user_controller",
-            controllerAs: "vm"
+    user = {
+        url: "/user",
+        views: {
+            "main": {
+                templateUrl: "routes/user/index.html",
+                controller: "user_controller",
+                controllerAs: "vm"
+            }
         }
-    }
-}
+    };
 
-
-var message = {
-    url: "/message",
-    views: {
-        "main": {
-            templateUrl: "routes/message/index.html",
-            controller: "message_controller",
-            controllerAs: "vm"
+    message = {
+        url: "/message",
+        views: {
+            "main": {
+                templateUrl: "routes/message/index.html",
+                controller: "message_controller",
+                controllerAs: "vm"
+            }
         }
-    }
-}
+    };
+})(angular);
