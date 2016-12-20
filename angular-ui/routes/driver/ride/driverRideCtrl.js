95903430f7ab90a0ba1d1fe2980073184d1c4f96
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-20 15:02:59 -0500

    Update driver ride controller to contain the current week's date.

--- angular-ui/routes/driver/ride/driverRideCtrl.js
+++ angular-ui/routes/driver/ride/driverRideCtrl.js
@@ -1,13 +1,17 @@
 (function(ng) {
     ng.module("revashare")
-    .controller("driverRideCtrl", ["$stateParams", "rideDataService", function($stateParams, rideDataService) {
+    .controller("driverRideCtrl", ["$stateParams", "$cookies", "rideDataService", "dateService", function($stateParams, $cookies, rideDataService, dateService) {
         var vm = this;
 
+        vm.title = "Create Ride - " + ($stateParams.toWork ? "To Work" : "From Work");
         vm.data = {};
 
         vm.createRide = function() {
             var ride = vm.data;
+            ride.username = $cookies.get("username");
             ride.isToWork = $stateParams.toWork;
+            ride.startOfWeekDate = dateService.getThisWeeksDate();
+            ride.isOnTime = true;
 
             if (ride.departureTime === null) {
                 return "Departure time cannot be empty.";
