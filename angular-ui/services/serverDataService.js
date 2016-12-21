ea89732fce4fab58bc0d0e55fc25eac8921804e0
Harley Wang <harleywwang@gmail.com>
2016-12-21 14:41:37 -0500

    refactor user functions move to userdataservice

--- angular-ui/services/serverDataService.js
+++ angular-ui/services/serverDataService.js
@@ -42,51 +42,6 @@
 
     }
 
-    function addUser (user, successCallback, errorCallback) {
-      $http({
-        method: "POST",
-        url: "/admin/adduser",
-        data: { 'key': user },
-        cache: false
-      })
-      .then(function success(response) {
-        successCallback(response.data);
-      },
-      function error(response) {
-        errorCallback("error");
-      });
-    }
-
-    function modifyUser (successCallback, errorCallback) {
-      $http({
-        method: "POST",
-        url: "/admin/modifyuser",
-        data: { 'key': user },
-        cache: false
-      })
-      .then(function success(response) {
-        successCallback(response.data);
-      },
-      function error(response) {
-        errorCallback("error");
-      });
-    }
-
-    function removeUser (successCallback, errorCallback) {
-      $http({
-        method: "POST",
-        url: "/admin/removeuser",
-        data: { 'key': user },
-        cache: false
-      })
-      .then(function success(response) {
-        successCallback(response.data);
-      },
-      function error(response) {
-        errorCallback
-      });
-    }
-
     // driver only
     function createSchedule (successCallback, errorCallback) {
 
