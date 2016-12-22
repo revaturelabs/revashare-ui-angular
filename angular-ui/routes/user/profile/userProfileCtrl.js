37ef712f2f1dc1e6b06aaa966c7b217fc1e43d8f
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-22 16:15:18 -0500

    Add index view and routes for user profile controller.

--- angular-ui/routes/user/profile/userProfileCtrl.js
+++ angular-ui/routes/user/profile/userProfileCtrl.js
@@ -1,8 +1,19 @@
 (function(ng) {
     ng.module("revashare")
-    .controller("userProfileCtrl", ["$state", "$cookies", "userDataService", function($state, $cookies, userDateService) {
+    .controller("userProfileCtrl", ["$state", "$cookies", "userDataService", function($state, $cookies, userDataService) {
         var vm = this;
-
+        vm.data = {};
         
+        userDataService.getUser($cookies.getObject("username"), function(user) {
+            vm.data.user = user;
+        }, function() {
+            console.log("Could not get user...");
+        });
+
+        if ($state.current.data.action == "edit") {
+            vm.updateProfile = function() {
+                // TODO
+            };
+        }
     }]);
 })(angular);
