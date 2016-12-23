4b1403b31db534bb2f4ba416b000d6887dd24c04
Harley Wang <harleywwang@gmail.com>
2016-12-23 11:55:27 -0500

    add admin comment review system

--- angular-ui/services/pendingUserService.js
+++ angular-ui/services/pendingUserService.js
@@ -5,6 +5,9 @@
 		this.approveDriver = approveDriver;
 		this.getPendingDrivers = getPendingDrivers;
 		this.getPendingUsers = getPendingUsers;
+		this.getUserComments = getUserComments;
+		this.removeComment = removeComment;
+		this.demoteDriver = demoteDriver;
 
 		function approveUser(user, successfulCallback, errorCallback) {
 			$http({
@@ -58,6 +61,53 @@
 			$http({
 				method: 'GET',
 				url: REVASHARE_API_URL + 'api/admin/get-pending-users',
+				cache: true
+			})
+			.then(
+				function success(response) {
+					successfulCallback(response.data);
+				},
+				function error() {
+					errorCallback('error');
+				});
+		}
+
+		function getUserComments(successfulCallback, errorCallback) {
+			$http({
+				method: 'GET',
+				url: REVASHARE_API_URL + 'api/admin/get-user-reports',
+				cache: true
+			})
+			.then(
+				function success(response) {
+					successfulCallback(response.data);
+				},
+				function error() {
+					errorCallback('error');
+				});
+		}
+
+		function removeComment(comment, successfulCallback, errorCallback) {
+			$http({
+				method: 'POST',
+				url: REVASHARE_API_URL + 'api/admin/remove-report',
+				data: comment,
+				cache: true
+			})
+			.then(
+				function success(response) {
+					successfulCallback(response.data);
+				},
+				function error() {
+					errorCallback('error');
+				});
+		}
+
+		function demoteDriver(comment, successfulCallback, errorCallback) {
+			$http({
+				method: 'POST',
+				url: REVASHARE_API_URL + 'api/admin/remove-driver-privileges',
+				data: comment,
 				cache: true
 			})
 			.then(
