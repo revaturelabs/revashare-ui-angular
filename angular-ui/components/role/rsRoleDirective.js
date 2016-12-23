070bb8a55269fc384d6b88cb6aa14de8ec575f59
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-23 10:22:42 -0500

    Add user role checking inside of the user data service.

--- angular-ui/components/role/rsRoleDirective.js
+++ angular-ui/components/role/rsRoleDirective.js
@@ -1,6 +1,25 @@
 (function(ng) {
     ng.module("revashare")
-    .directive("rsRoleGroup", [function() {
+    .directive("rsRoleGroup", ["displayStateService", function(displayStateService) {
+        return {
+            restrict: "E",
+            transclude: true,
+            template: '<div ng-transclude="ng-transclude"></div>',
+            link: function(scope, element, attrs) {
+                var roles = attrs.rsRoles;
 
+                if (roles === undefined) {
+                    roles = [];
+                }
+                else {
+                    roles = roles.replace(" ", "");
+                    roles = roles.split(",");
+                }
+
+                element.css({
+                    display: "none"
+                });
+            }
+        };
     }]);
 })(angular);
--- angular-ui/services/userDataService.js
+++ angular-ui/services/userDataService.js
@@ -1,11 +1,48 @@
 (function() {
 	angular.module('revashare')
-	.service('userDataService', ['$http', 'REVASHARE_API_URL', function($http, REVASHARE_API_URL) {
+	.service('userDataService', ['$http', '$state', '$cookies', 'REVASHARE_API_URL', function($http, $state, $cookies, REVASHARE_API_URL) {
 		this.addUser = addUser;
     this.getDrivers = getDrivers;
     this.getRiders = getRiders;
 		this.modifyUser = modifyUser;
 		this.removeUser = removeUser;
+    this.isInGroup = isInGroup;
+    this.redirectIfNotInGroup = redirectIfNotInGroup;
+
+    var cache = {};
+
+    function isInGroup(groups) {
+      var isIn = false;
+      var user = getLoggedInUser();
+      var userRole = user.Roles[0].Name;
+
+      angular.forEach(groups, function(group) {
+        if (group === userRole) {
+          isIn = true;
+        }
+      });
+
+      return isIn;
+    }
+
+    function redirectIfNotInGroup(groups, redirectState, redirectParams) {
+      if (redirectParams === undefined) {
+        redirectParams = {};
+      }
+
+      if (!isInGroup(groups)) {
+        $state.go(redirectState, redirectParams);
+      }
+    }
+
+    function getLoggedInUser() {
+      if (cache.loggedInUsername !== $cookies.getObject("username")) {
+        cache.loggedInUsername = $cookies.getObject("username")
+        cache.loggedInUser = getUser(cache.loggedInUsername);
+      }
+
+      return cache.loggedInUser;
+    }
 
     function addUser (user, successCallback, errorCallback) {
       $http({
