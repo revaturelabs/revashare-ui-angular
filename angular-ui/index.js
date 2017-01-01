99e8d085e32bfb000eba05a070870ff33417bc02
Richard Voelker <richard.voelker.software@gmail.com>
2017-01-01 12:46:44 -0500

    Add angular loading bar.

--- angular-ui/index.js
+++ angular-ui/index.js
@@ -1,7 +1,8 @@
 
 var app = angular.module("revashare", [
 	"ui.router",
-	"ngCookies"
+	"ngCookies",
+    "angular-loading-bar"
 	])
 .constant("REVASHARE_API_URL", "http://54.145.144.187/revashare-api/")
 .config(["$httpProvider", function($httpProvider)
