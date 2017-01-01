ff8013d33278e03b9a4c14abfee6c5b7481da859
Richard Voelker <richard.voelker.software@gmail.com>
2017-01-01 14:27:42 -0500

    Add rsDisabled to login form submit button to prevent multiple submissions.

--- angular-ui/routes/login/login_controller.js
+++ angular-ui/routes/login/login_controller.js
@@ -4,15 +4,18 @@ angular.module('revashare').controller('login_controller', ['displayStateService
   vm.login = login;
   vm.recoverPassword = recoverPassword;
   vm.loginForm = {};
+  vm.formDisabled = false;
 
   function login() {
     loginService.login(vm.loginForm.username, vm.loginForm.password,
       function success(user) {
         window.toastr.success('Welcome, ' + vm.loginForm.username + '!');
         displayStateService.alert_logged_in(user);
+        vm.formDisabled = false;
       },
       function error () {
         window.toastr.error('We could not login user ' + vm.loginForm.username + '. Please try again.');
+        vm.formDisabled = false;
       });
   }
 
