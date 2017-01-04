33d006e1911f787429644d67506e853e6736fa6b
Richard Voelker <richard.voelker.software@gmail.com>
2017-01-04 11:50:06 -0500

    Add demo shortcuts to login controller.

--- angular-ui/routes/login/login_controller.js
+++ angular-ui/routes/login/login_controller.js
@@ -1,10 +1,45 @@
-angular.module('revashare').controller('login_controller', ['displayStateService', 'loginService', function (displayStateService, loginService) {
+angular.module('revashare').controller('login_controller', ['$document', '$scope', 'displayStateService', 'loginService', function ($document, $scope, displayStateService, loginService) {
   var vm = this;
 
   vm.login = login;
   vm.recoverPassword = recoverPassword;
   vm.loginForm = {};
   vm.formDisabled = false;
+
+  // -- ONLY FOR DEMONSTRATION PURPOSES; REMOVE LATER
+  $document.on("keypress", function(event) {
+    var key = String.fromCharCode(event.which);
+    if (key === "0") {
+      $scope.$apply(function() {
+        vm.loginForm = {};
+      });
+    }
+    else if (key === "2") {
+      $scope.$apply(function() {
+        vm.loginForm = {
+          username: "demo_user",
+          password: "password"
+        };
+      });
+    }
+    else if (key === "4") {
+      $scope.$apply(function() {
+        vm.loginForm = {
+          username: "rider",
+          password: "password"
+        };
+      });
+    }
+    else if (key === "6") {
+      $scope.$apply(function() {
+        vm.loginForm = {
+          username: "driver",
+          password: "password"
+        };
+      });
+    }
+  });
+  // -- ONLY FOR DEMONSTRATION PURPOSES; REMOVE LATER
 
   function login() {
     loginService.login(vm.loginForm.username, vm.loginForm.password,
