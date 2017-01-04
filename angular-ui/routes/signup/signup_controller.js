e72e5a313eb8c3f581da13aca95bc2dc3fb9d314
Richard Voelker <richard.voelker.software@gmail.com>
2017-01-04 11:50:28 -0500

    Add demo shortcuts to signup controller

--- angular-ui/routes/signup/signup_controller.js
+++ angular-ui/routes/signup/signup_controller.js
@@ -1,10 +1,34 @@
-angular.module('revashare').controller('signup_controller', ['registrationService', 'apartmentDataService', '$state', function (registrationService, apartmentDataService, $state) {
+angular.module('revashare').controller('signup_controller', ['$scope', '$document', 'registrationService', 'apartmentDataService', '$state', function ($scope, $document, registrationService, apartmentDataService, $state) {
   var vm = this;
 
   vm.apartments = [];
 
   vm.listApartments = listApartments;
   vm.signUp = signUp;
+
+  // -- ONLY FOR DEMONSTRATION PURPOSES; REMOVE LATER
+  $document.on("keypress", function(event) {
+    var key = String.fromCharCode(event.which);
+    if (key === "0") {
+      $scope.$apply(function() {
+        vm.user = {};
+        vm.password = "";
+      });
+    }
+    else if (key === "1") {
+      $scope.$apply(function() {
+        vm.user = {
+          UserName: "mohbrien",
+          Name: "Matt O'Brien",
+          Email: "ohbrien93@hotmail.com",
+          PhoneNumber: "(555) 867 - 5309",
+          Apartment: vm.apartments[5]
+        };
+        vm.password = "password";
+      });
+    }
+  });
+  // -- ONLY FOR DEMONSTRATION PURPOSES; REMOVE LATER
 
   listApartments();
 
